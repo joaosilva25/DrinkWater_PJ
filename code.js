2ba572156a3f033706b3098b86e27c7739f7a35a
Joao <joaosilva2505@outlook.comgit config --global user.name joaosilva25>
2023-05-28 14:04:41 -0300

    (second commit)

--- code.js
+++ code.js
@@ -1,16 +1,48 @@
+let nivel_water=document.querySelector('.nivel_water_intern')
+let mensage_for_user=document.querySelector('p')
+
 let acumulative=0
 
 function clicar() {
     let cup = 250;
     let bottom = 2000;
     let res1 = (cup / bottom) * 100;
+    acumulative +=res1;
+    
+    if (acumulative<=100) {
+    let porcent=document.querySelector('h5')
+    porcent.innerHTML=`<h5>${acumulative}%</h5>`;
+    }
 
-    while (res1 <= 100) {
-        res1 = (cup / bottom) * 100; 
-        acumulative+=res1;
-        cup += 250; 
-
-        let porcent = document.querySelector('h5');
-        porcent.innerHTML = `<h5>${acumulative}%</h5>`;
+    /*Nivel of Water*/
+    if (acumulative==12.5) {
+        nivel_water.style.height='12.5%';
+        mensage_for_user.innerHTML='<p>Você bebeu pouca água. Tente beber mais!</p>'
+    }
+    else if (acumulative==25) {
+        nivel_water.style.height='25%';
+        mensage_for_user.innerHTML='<p>Você está no caminho certo, mas pode beber um pouco mais.</p>'
+    }
+    
+    else if (acumulative==37.5) {
+        nivel_water.style.height='37.5%';
+    }
+    else if (acumulative==50) {
+        nivel_water.style.height='50%';
+        mensage_for_user.innerHTML='<p>Ótimo! Você está hidratado(a). Continue assim!</p>'
+    }
+    else if (acumulative==62.5) {
+        nivel_water.style.height='62.5%';
+    }
+    else if (acumulative==75) {
+        nivel_water.style.height='75%';
+        mensage_for_user.innerHTML='<p>Você já bebeu bastante água hoje. Parabéns!</p>'
+    }
+    else if (acumulative==87.5) {
+        nivel_water.style.height='87.5%';
+    }
+    else if (acumulative==100) {
+        nivel_water.style.height='100%';
+        mensage_for_user.innerHTML='<p>Você conseguiu vencer o desafio estamos orgulhos de você!</p>'
     }
 }
